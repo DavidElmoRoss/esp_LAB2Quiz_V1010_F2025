e121d402cce4d8c090030b04cd45c94ba373b18c
David Elmo Ross <david.elmo.ross@icloud.com>
2025-06-29 15:20:03 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/*
+When you BUILD and UPLOAD then RUN this code in PLATFORMIO,
+HELLO WORLD is centred on the screen in _________ letters
+on a __________ Background.
+*/
+
+
+#include "arduino.h"
+
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+    printf("\e[1;1H\e[2J");
+    printf("\e[1;33;44m");  
+    fflush(stdout);
+    printf("\e[12;35H");
+    printf("Hello World");
+    fflush(stdout);
+    for(;;);
+}
